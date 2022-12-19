875ee317bfea5a3499dbaa0e53568db263a934eb
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2022-12-19 14:38:20 -0600

    componentes
    ya se pudo, no pregunten

---
+++ client/src/Components/Carta/Carta.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Carta = () => {
+  return (
+    <div>
+      <h1>Carta</h1>
+    </div>
+  );
+};
+
+export default Carta;
---
+++ client/src/Components/MostrarCartas/MostrarCartas.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MostrarCartas = () => {
+  return (
+    <div>
+      <h1>Aqui se van a renderizar todas las cartas</h1>
+    </div>
+  );
+};
+
+export default MostrarCartas;
