bd27263b5f61aa49d5f55125dee1b78aec7b01e0
Pedro Munoz <Pedrojose0272@gmail.com>
2022-12-19 21:28:27 -0500

    Revert "Add form to Login Component and styles"
    This reverts commit 90bcca23186feb4b5b166ac0ca101cf0e181a8cc.

--- client/src/Components/FormLogin/FormLogin.jsx
+++ client/src/Components/FormLogin/FormLogin.jsx
@@ -1,27 +1,9 @@
 import React from "react";
-import styles from "./FormLogin.module.css";
 
 const FormLogin = () => {
   return (
     <div>
-      <form className={styles.container}>
-        <h2 className={styles.title}>SIGN IN</h2>
-
-        <input
-          placeholder="Email"
-          className={styles.input}
-          type="text"
-          name="email"
-        />
-
-        <input
-          placeholder="Password"
-          className={styles.input}
-          type="password"
-          name="password"
-        />
-        <button className={styles.btn}>Log In</button>
-      </form>
+      <h1>Formulario login</h1>
     </div>
   );
 };
