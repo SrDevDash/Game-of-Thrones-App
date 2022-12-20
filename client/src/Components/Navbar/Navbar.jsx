304571f62a1fff2ff885cf9baeddb10c5a005827
Pedro Munoz <Pedrojose0272@gmail.com>
2022-12-20 11:11:30 -0500

    add navbar and styles of navbar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Barra de Navegación</h1>
+    <div className={styles.container}>
+      <div className={styles.littleContainer}>
+        <Link to="/">
+          <p>Home</p>
+        </Link>
+        <button>Filter</button>
+      </div>
+      <div className={styles.littleContainer}>
+        <div>
+          <input type="text" name="searchInput" />
+          <button>Search</button>
+        </div>
+        <Link to="/login">
+          <p>Sign out</p>
+        </Link>
+      </div>
     </div>
   );
 };
