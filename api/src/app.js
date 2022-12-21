9867a9058f32a2d11cf17c8e1eb73e46189878b7
Pedro Munoz <Pedrojose0272@gmail.com>
2022-12-21 15:00:11 -0500

    adding cors to server
    Cors if to break limitations for localhost
    
    if you dont have cors you cant use this server on the front

--- api/src/app.js
+++ api/src/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { json } = require("body-parser");
 const singUpRouter = require("./routers/singupRouter");
 const loginRouter = require("./routers/loginRouter");
+const cors = require('cors')
 
 const server = express();
 
 server.name = "API";
+server.use(cors());
 
 server.use(express.json());
 
